1f34cc21d062948d8e0a4a101bbe6454a6b8bc6d
Sean O'Leary <sean@soleary.com>
2017-09-28 22:21:32 -0400

    Make an invoices table Google Mail Merge can use

---
+++ invoices.sql
@@ -0,0 +1,16 @@
+create table invoices(
+     "First Name" text not null
+    ,"Last Name" text not null
+    ,"Email Address" text not null
+    ,total_tuition text not null
+    ,payment_plan text not null
+    ,cash_amount text not null
+    ,paypal_amount text not null
+    ,paypal_url text not null
+    ,billing_date text not null
+    ,invoice_date text
+    ,testing text
+    ,"File Attachments" text
+    ,"Scheduled Date" text
+    ,"Mail Merge Status" text
+);
