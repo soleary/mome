e67888eb7be7edb4f3d17f3405dafbfdd1da33fb
Sean O'Leary <sean@soleary.com>
2017-09-28 22:24:37 -0400

    Create a table to store payment information

---
+++ payments.sql
@@ -0,0 +1,8 @@
+create table payments(
+     email text not null
+    ,payment_date text not null
+    ,method text not null
+    ,payment_amount text not null
+    ,validation text
+    ,testing text
+);
