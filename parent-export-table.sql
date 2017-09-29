90c95e327427204c5202c2f3e3f484532260a61a
Sean O'Leary <sean@soleary.com>
2017-09-28 22:20:26 -0400

    Format them the way the Oracle guys taught me

--- parent-export-table.sql
+++ parent-export-table.sql
@@ -1,9 +1,9 @@
 drop table parent_export;
 CREATE TABLE parent_export(
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "phone_number" TEXT NOT NULL,
-    "address" TEXT,
-    "payment_schedule" TEXT NOT NULL
+    "first_name" TEXT NOT NULL
+    ,"last_name" TEXT NOT NULL
+    ,"email" TEXT NOT NULL
+    ,"phone_number" TEXT NOT NULL
+    ,"address" TEXT
+    ,"payment_schedule" TEXT NOT NULL
 );
--- student-export-table.sql
+++ student-export-table.sql
@@ -1,8 +1,8 @@
 drop table student_export;
 CREATE TABLE student_export(
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "homeroom" TEXT,
-    "class" TEXT,
-    "instrument" TEXT
+     "first_name" TEXT NOT NULL
+    ,"last_name" TEXT NOT NULL
+    ,"homeroom" TEXT
+    ,"class" TEXT
+    ,"instrument" TEXT
 );
