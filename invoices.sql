634ccda694ff89748c5c3add401779a994733481
Sean O'Leary <sean@soleary.com>
2017-10-20 00:08:30 -0400

    Added new fields for proper invoicing
    debit -> how much this account should be debited
    for this invoice period.  This is the column to use
    to add up all the invoices to see what the user has been billed.
    Individual invoice amounts can be higher if the family falls
    behind or smaller if they are ahead, but this amount must be the
    amount owed for this invoice period in order for invoices and payments
    to add up.
    
    adjust -> Notification to the mail-merging person to possibly adjust
    this invoice due to an outstanding balance or a change in the family's
    tuition or payment plan

--- invoices.sql
+++ invoices.sql
@@ -5,12 +5,14 @@ create table invoices(
     ,total_tuition text not null
     ,payment_plan text not null
     .amount text not null,
+    ,debit text not null,
     ,cash_amount text not null
     ,paypal_amount text not null
     ,paypal_url text not null
     ,billing_date text not null
     ,invoice_date text
     ,testing text
+    ,adjust text
     ,"File Attachments" text
     ,"Scheduled Date" text
     ,"Mail Merge Status" text
