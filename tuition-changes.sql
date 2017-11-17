b58688dafb8a6780f47c4b8bf65ab064c80f3353
Sean O'Leary <sean@soleary.com>
2017-11-16 23:00:43 -0500

    Allow user to adjust the date range

--- tuition-changes.sql
+++ tuition-changes.sql
@@ -1,11 +1,13 @@
 select 
      "Email Address"
+    ,invoice_date
     ,total_tuition 
 from invoices 
 where "Email Address" in (
     select 
         "Email Address" 
     from invoices 
+    where invoice_date > '2017-10-01'
     group by "Email Address" 
     having count(distinct(total_tuition)) > 1) 
 order by "Email Addess";
