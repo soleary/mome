7d2ad1e192327fa3f669065b24ebc6a5f9c9e55d
Sean O'Leary <sean@soleary.com>
2019-06-13 19:37:53 -0400

    Convert month numbers (zero-based) to proper names

--- sql/prettify.sql
+++ sql/prettify.sql
@@ -25,6 +25,20 @@ values
 ('dow', 5, 'Friday'   ),
 ('dow', 6, 'Saturday' ),
 
+/* Month Names */
+('month', 01, 'January'),
+('month', 02, 'February'),
+('month', 03, 'March'),
+('month', 04, 'April'),
+('month', 05, 'May'),
+('month', 06, 'June'),
+('month', 07, 'July'),
+('month', 08, 'August'),
+('month', 09, 'September'),
+('month', 10, 'October'),
+('month', 11, 'November'),
+('month', 12, 'December'),
+
 /* English affirmative-negative */
 ('a-n', 'yes', 'Yes'),
 ('a-n', 'no', 'No'  ),
