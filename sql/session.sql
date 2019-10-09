d51e2ba3aa365fa0d9e798355db085ee00267231
Sean O'Leary <sean@soleary.com>
2019-10-09 13:15:25 -0400

    Session and schedule for new school year

--- sql/session.sql
+++ sql/session.sql
@@ -2,11 +2,13 @@
  * Active should only be set for the "current" session
  */
 
+drop table if exists session;
+
 create table session(
      id INTEGER PRIMARY KEY
     ,name text not null
     ,active integer
 );
 
-insert into session (name, active) values ('2018-2019 School Year', 1);
-insert into session (name, active) values ('Summer 2019', null);
+insert into session (name, active) values ('2018-2019 School Year', null);
+insert into session (name, active) values ('2019-2020 School Year', 1);
