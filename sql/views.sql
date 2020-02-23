10acdf2e8cccd33148a74bce2a464d3e2426750a
Sean O'Leary <sean@soleary.com>
2020-02-23 14:55:30 -0500

    Transition to new views
    This helps consolidate the logic for determining which families and
    people are in the active session and which students are active.
    
    By creating these views with all of the constraints in one place,
    we can easily change the definition of these things and we can
    use that logic appropriately anywhere we need it in the programs.
    
    Billing people are one idea of an "active" person.  This definition
    should be used when trying to apply payments or look up families that
    may owe.  Any logic needed in a financial sense should use billing_*.
    
    Ed_student is used to see who's an "active" student from a educational
    standpoint.  When determining things like which parents should appear
    on which class list or if a family has any active students, this view
    should be used as it is sensitive to kids withdrawing and such.
    
    Basically once a family is signed up, they are a billing_family and will
    stay one for the duration of their session.  Students can come and go.

--- sql/views.sql
+++ sql/views.sql
@@ -1,22 +1,54 @@
+/* Convenience view for the active_session */
+drop view if exists active_session;
+create view active_session as
+    select * from session where active is not null;
+
+/* Active family for billing purposes.
+ * Any family from the active session. */
+drop view if exists billing_family;
+create view billing_family as
+    select * from family
+    where momefid in (select momefid from family where session = (select id from active_session));
+
+/* Active people for billing purposes.
+ * Any person that is in a family from the active session. */
+drop view if exists billing_person;
+create view billing_person as
+    select f.momefid, p.id as personid, p.type, p.firstname, p.lastname, p.phone, p.email, p.grade, p.homeroom
+    from person as p, family as f, family_member as fm
+    where   p.id = fm.personid
+        and fm.momefid = f.momefid
+        and f.session = (select id from active_session)
+    order by p.lastname, p.id;
+
+/* Active students for educational purposes.
+ * Only students that are in the active session and active in at least one class.
+ * Only students can be a class_member, so no need to limit to person.type = 'student' */
+drop view if exists ed_student;
+create view ed_student as
+    select *
+    from class_member
+    where   personid in (select id from billing_person)
+        and inactive is null;
+
 drop view if exists payment;
 create view payment as
     select * from ledger
     where (type != 'debit' or type != 'credit' or type != 'refund')
         and validated is not null
-        and momefid in (select momefid from active_family);
+        and momefid in (select momefid from billing_family);
 
 drop view if exists total_payment;
 create view total_payment as
     select f.*, coalesce(sum(p.amount), 0) as total_payment
-        from family as f left outer join payment as p on (f.momefid = p.momefid)
-        where f.session = (select id from active_session)
+        from billing_family as f left outer join payment as p on (f.momefid = p.momefid)
         group by f.momefid;
 
 drop view if exists debit;
 create view debit as
     select * from ledger
     where type = 'debit'
-        and momefid in (select momefid from active_family);
+        and momefid in (select momefid from billing_family);
 
 drop view if exists clean_ledger;
 create view clean_ledger as
@@ -85,18 +117,19 @@ create view deposit_slip as
         from deposits
         order by type desc;
 
+/* Use ed_student, so we only end up with active students */
 drop view if exists student_roster;
 create view student_roster as
-    select f.momefid, cm.id as classid, p.id as personid, p.firstname, p.lastname, p.grade, p.homeroom, cm.class, cm.instrument, cm.experience, pr.o as day, cm.inactive
-    from person as p, class_member as cm, family as f, family_member as fm, prettify as pr
+    select f.momefid, cm.id as classid, p.id as personid, p.firstname, p.lastname, p.grade, p.homeroom, cm.class, cm.instrument, cm.experience, pr.o as day, cm.day as dow, cm.inactive
+    from person as p, ed_student as cm, billing_family as f, family_member as fm, prettify as pr
     where p.id = cm.personid
         and f.momefid = fm.momefid
-        and f.session = (select id from active_session)
         and fm.personid = p.id
-        and cm.inactive is null
         and (pr.type = 'dow' and pr.i = cm.day)
     order by p.lastname, p.firstname;
 
+/* Based on the student roster, so we don't list parents that don't have at
+ * least one active student in their family. */
 drop view if exists parent_roster;
 create view parent_roster as
     select f.momefid, p.id, p.firstname, p.lastname, p.phone, p.email
@@ -104,59 +137,31 @@ create view parent_roster as
     where p.id = fm.personid
         and fm.momefid = f.momefid
         and p.type = 'parent'
-        and f.session = (select id from active_session)
+        and f.momefid in (select momefid from student_roster)
     order by p.lastname, p.id;
-
-
-drop view if exists active_session;
-create view active_session as
-    select * from session where active is not null;
-
-drop view if exists active_family;
-create view active_family as
-    select * from family
-    where session = (select id from active_session);
-
-drop view if exists active_family_member;
-create view active_family_member as
-    select * from family_member
-    where momefid in (select momefid from active_family);
-
-drop view if exists active_person;
-create view active_person as
-    select f.momefid, p.id, p.type, p.firstname, p.lastname, p.phone, p.email
-    from person as p, family as f, family_member as fm
-    where p.id = fm.personid
-        and fm.momefid = f.momefid
-        and f.session = (select id from active_session)
-    order by p.lastname, p.id;
-
-drop view if exists active_class_member;
-create view active_class_member as
-    select * from class_member where personid in (select id from active_person) and inactive is null;
 
 drop view if exists total;
 create view total as
-      select 01 as Num, 'Tuition         ' as Item, printf("%.2f", sum(tuition)) as 'Total' from family where momefid in (select momefid from active_family)
+      select 01 as Num, 'Tuition         ' as Item, printf("%.2f", sum(tuition)) as 'Total' from family where momefid in (select momefid from billing_family)
 union select 02, 'Owed', printf("%.2f", sum(owed)) from tuition_remaining
 union select 03, 'Paid', printf("%.2f", sum(amount)) as 'Total' from payment
 union select 04, 'Billed', printf("%.2f", sum(amount)) from debit
-union select 05, 'Balance', printf("%.2f", sum(amount)) from clean_ledger where momefid in (select momefid from active_family)
-union select 06, 'High', printf("%.2f", max(balance)) from balance where momefid in (select momefid from active_family)
-union select 07, 'Low', printf("%.2f", min(balance)) from balance where momefid in (select momefid from active_family)
-union select 08, 'Families', count(id) from family where session = (select id from active_session)
-union select 09, 'Parents', count(id) from person where type = 'parent' and id in (select personid from active_family_member)
-union select 10, 'Students', count(id) from person where type = 'student' and id in (select personid from active_family_member)
-union select 11, 'Payers', count(id) from person where type = 'payer' and id in (select personid from active_family_member)
-union select 12, 'Paid Up', count(*) from paid_up where momefid in (select momefid from active_family)
-union select 13, 'Zero Balance', count(*) from balance where balance = 0 and momefid in (select momefid from active_family)
-union select 14, 'Negative Balance', count(*) from balance where balance < 0 and momefid in (select momefid from active_family)
-union select 15, 'Positive Balance', count(*) from balance where balance > 0 and momefid in (select momefid from active_family)
-union select 16, 'Invoices', count(id) from invoice where session = (select id from active_session)
-union select 17, 'Notifications', count(id) from notification where sentdate is not null and momefid in (select momefid from active_family)
-union select 18, 'Payments', count(id) from ledger where type != 'adjustment' and type != 'debit' and momefid in (select momefid from active_family)
-union select 19, 'Debits', count(id) from ledger where type = 'debit' and momefid in (select momefid from active_family)
-union select 20, 'Transactions', count(id) from ledger where momefid in (select momefid from active_family)
+union select 05, 'Balance', printf("%.2f", sum(amount)) from clean_ledger where momefid in (select momefid from billing_family)
+union select 06, 'High', printf("%.2f", max(balance)) from balance where momefid in (select momefid from billing_family)
+union select 07, 'Low', printf("%.2f", min(balance)) from balance where momefid in (select momefid from billing_family)
+union select 08, 'Families', count(id) from billing_family
+union select 09, 'Parents', count(id) from parent_roster
+union select 10, 'Students', count(id) from student_roster
+union select 11, 'Payers', count(id) from billing_person where type = 'payer'
+union select 12, 'Paid Up', count(*) from paid_up where momefid in (select momefid from billing_family)
+union select 13, 'Zero Balance', count(*) from balance where balance = 0 and momefid in (select momefid from billing_family)
+union select 14, 'Negative Balance', count(*) from balance where balance < 0 and momefid in (select momefid from billing_family)
+union select 15, 'Positive Balance', count(*) from balance where balance > 0 and momefid in (select momefid from billing_family)
+union select 16, 'Invoices', count(id) from invoice where momefid in (select momefid from billing_family)
+union select 17, 'Notifications', count(id) from notification where sentdate is not null and momefid in (select momefid from billing_family)
+union select 18, 'Payments', count(id) from ledger where type != 'debit' and momefid in (select momefid from billing_family)
+union select 19, 'Debits', count(id) from ledger where type = 'debit' and momefid in (select momefid from billing_family)
+union select 20, 'Transactions', count(id) from ledger where momefid in (select momefid from billing_family)
 order by Num;
 
 drop view if exists grand_total;
